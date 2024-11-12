851a01ba01b2c104769363736bab4f35684f23d0
Ajay Singh <ajaysingh9982374320@gmail.com>
2024-11-12 09:23:04 +0530

    updated to latest

--- errs/errors.go
+++ errs/errors.go
@@ -1,37 +1,49 @@
 package errs
 
-import (
-	"net/http"
-)
+import "net/http"
 
-type AppError struct{
-	Code int        `json:",omitempty"`
-	Message string  `json:"message"`
+type AppError struct {
+	Code    int    `json:",omitempty"`
+	Message string `json:"message"`
 }
 
-func (e *AppError) AsMessage() *AppError{
+func (e AppError) AsMessage() *AppError {
 	return &AppError{
 		Message: e.Message,
 	}
 }
 
-func NewNotFoundError(message string) *AppError{
+func NewNotFoundError(message string) *AppError {
 	return &AppError{
 		Message: message,
-		Code: http.StatusNotFound,
+		Code:    http.StatusNotFound,
 	}
 }
 
-func NewUnexpectedError(message string) *AppError{
+func NewUnexpectedError(message string) *AppError {
 	return &AppError{
 		Message: message,
-		Code: http.StatusInternalServerError,
+		Code:    http.StatusInternalServerError,
 	}
 }
 
-func NewValidationError(message string) *AppError{
+func NewValidationError(message string) *AppError {
 	return &AppError{
 		Message: message,
-		Code: http.StatusUnprocessableEntity, 
+		Code:    http.StatusUnprocessableEntity,
+	}
+}
+
+func NewAuthenticationError(message string) *AppError {
+	return &AppError{
+		Message: message,
+		Code:    http.StatusUnauthorized,
+	}
+}
+
+func NewAuthorizationError(message string) *AppError {
+	return &AppError{
+		Message: message,
+		Code:    http.StatusForbidden,
 	}
 }
--- logger/logger.go
+++ logger/logger.go
@@ -5,10 +5,10 @@ import (
 	"go.uber.org/zap/zapcore"
 )
 
-
 var log *zap.Logger
 
-func init(){
+func init() {
+	var err error
 
 	config := zap.NewProductionConfig()
 
@@ -16,25 +16,27 @@ func init(){
 	encoderConfig.TimeKey = "timestamp"
 	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
 	encoderConfig.StacktraceKey = ""
-
 	config.EncoderConfig = encoderConfig
 
-	var err error
-	log,err =  config.Build(zap.AddCallerSkip(1))
-	if(err != nil){
+	log, err = config.Build(zap.AddCallerSkip(1))
+
+	if err != nil {
 		panic(err)
 	}
-
 }
 
-func Info(message string, fields ...zap.Field){
-	log.Info(message,fields...)
+func Info(message string, fields ...zap.Field) {
+	log.Info(message, fields...)
 }
 
-func Debug(message string, fields ...zap.Field){
-	log.Debug(message,fields...)
+func Fatal(message string, fields ...zap.Field) {
+	log.Fatal(message, fields...)
 }
 
-func Error(message string, fields ...zap.Field){
-	log.Error(message,fields...)
+func Debug(message string, fields ...zap.Field) {
+	log.Debug(message, fields...)
+}
+
+func Error(message string, fields ...zap.Field) {
+	log.Error(message, fields...)
 }
